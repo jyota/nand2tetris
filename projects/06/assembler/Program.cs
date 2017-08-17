f4d7a2c4d69f297fb7ff347512fa7ef8d3fc6b90
Jyota Snyder <jyotasnyder@Jyotas-MacBook-Pro.local>
2017-08-16 21:40:24 -0700

    beginnings of reading source file from command line argument.

--- projects/06/assembler/Program.cs
+++ projects/06/assembler/Program.cs
@@ -9,7 +9,19 @@ namespace assembler
             Parser myParser = new Parser();
             Code myCode = new Code();
             SymbolTable mySymbolTable = new SymbolTable();
-            Console.WriteLine(myParser.CallMe() + myCode.CallMeToo() + mySymbolTable.CallMeThree());
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Need assembly source code file as argument.");
+            }
+
+            System.IO.StreamReader file = new System.IO.StreamReader(args[0]);
+
+            String line;
+            while((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+            file.Close();
         }
     }
 }
